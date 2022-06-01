cd34373273bd379d6b9568e16c7846ac78dfe198
Todd Pangilinan <pang.todd@gmail.com>
2022-05-31 17:17:27 -0700

    establsih tests for many-to-many relationships between album and artists.

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe Album do
   it { should have_many(:songs) }
+  it { should have_and_belong_to_many :artists }
   it { should validate_presence_of :name}
   it { should validate_length_of(:name).is_at_most(100) }
   it("titleizes the name of an album")do
