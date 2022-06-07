fa87c764681c60777d743e8f3a79ce9c12e60d40
Todd Pangilinan <pang.todd@gmail.com>
2022-06-07 11:16:23 -0700

    added routes for signin, signup.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,12 @@ Rails.application.routes.draw do
   resources :albums do
     resources:songs
   end
+
+  get '/signup' => 'users#new'
+  post 'users' => 'users#create'
+
+  get '/signin' => 'sessions#new'
+  post '/signin' => 'session#create'
+  get '/signout' => 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
